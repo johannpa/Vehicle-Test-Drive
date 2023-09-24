74e05f589268ff72052acced9e3a953e83367605
johannpa <johann_paque@hotmail.fr>
2023-09-24 20:43:39 +0200

    Db context for Customers microservice

---
+++ CustomersApi/Data/ApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CustomersApi.Models;
+
+namespace CustomersApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=VehicleApiDb;");
+        }
+    }
+}
